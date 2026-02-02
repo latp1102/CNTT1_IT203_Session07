fb7bce345d93c59f9fb27b9f925ea24f994ac73b
latp1102 <Latp1102@gmail.com>
2026-02-02 23:37:31 +0700

    Refactor score validation and add output for limits
    Removed score validation and added output for max and min scores.

--- src/b5.java
+++ src/b5.java
@@ -2,11 +2,10 @@ public class b5 {
     private static final double MAX_SCORE = 10;
     private static final double MIN_SCORE = 0;
     public static void main(String[] args) {
-        double score = 4;
-        if(score >= b5.MIN_SCORE && score <= b5.MAX_SCORE){
-            System.out.println("Điểm hợp lệ");
-        }else {
-            System.out.println("không hợp lệ");
-        }
+        System.out.println("max: " + b5.MAX_SCORE);
+        System.out.println("min: " + b5.MIN_SCORE);
+        // b5.MAX_SCORE = 200; // Lỗi biên dịch vì final 
+        // là định nghĩa hằng số không được phép thay đổi
+        // Lỗi: cannot assign a value to final variable MAX_SCORE
     }
 }
